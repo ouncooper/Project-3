fe93126d96b3b3d13e83a9141320d4fa371192e0
Narkidae <connorbtaylor@gmail.com>
2013-10-09 14:11:40 -0500

    Javadoc, new methods

--- Graph.java
+++ Graph.java
@@ -1,63 +1,48 @@
-import java.io.Serializable;
-import java.util.Comparator;
 
-public class JournalArticle extends Paper implements Serializable, Comparator<Paper>
-{
-	private static final long serialVersionUID = 3324219143415691342L;
-	
-	/* A variable to store the volume and issue of the article. */
-	private String volumeIssue;
+public class Graph {
 	
 	/**
-	 * Default constructor for the class.
+	 * Default constructor for the class
 	 */
-	public JournalArticle() {}
+	public Graph() { }
 	
 	/**
-	 * Inherits from the Paper class.
-	 * Creates a JournalArticle Object
-	 * @param newName Name of the paper ("Journal Article").
-	 * @param newAuthors Authors of the paper in the format Last, First Middle; Last, First Middle; etc.
-	 * @param newTitle Title of the paper.
-	 * @param newSerialTitle Serial title of the paper.
-	 * @param newDate Date the paper was published.
-	 * @param newVolumeIssuePageRange Volume, issue, and page range specified in the file.  Split into two variables.
-	 * @param newDOI Digital Object Identifier for the paper.
-	 **/
-	public JournalArticle(String newName, String newAuthors, String newTitle, String newSerialTitle, String newVolumeIssuePageRange, String newDate, String newDOI)
-	{
-		setName(newName);
-		//setAuthors(newAuthors);
-		setTitle(newTitle);
-		setSerialTitle(newSerialTitle);
-		volumeIssue = newVolumeIssuePageRange.split(":")[0];
-		setPageRange(newVolumeIssuePageRange.split(":")[1]);
-		setDate(newDate);
-		setDOI(newDOI);
+	 * Graphs the number of journal articles vs the number of conference papers 
+	 * @param input The author who's data you want to graph
+	 */
+	public void graphType(Author input){
+		
 	}
 	
 	/**
-	 * Gets the volume and issue of the Journal article
-	 * @return a string containing the volume and issue in the format "volume(issue)"
+	 * Graphs the number of publications the author produced each year
+	 * @param input The author who's data you want to graph
 	 */
-	public String getVolumeIssue() {
-		return volumeIssue;
+	public void graphPublicationsPerYear(Author input){
+		
 	}
 	
 	/**
-	 * Converts the object to a string separated by " // "
+	 * Graphs the number of conference papers the author produced each year
+	 * @param input The author who's data you want to graph
 	 */
-	public String toString()
-	{
-		return getName() + " // " + getAuthors() + " // " + getTitle() + " // " + getSerialTitle() + " // " + getVolumeIssue() + ":" + getPageRange() + " // " + getDate() + " // " + getDOI();
+	public void graphConferencePerYear(Author input){
+		
 	}
 
 	/**
-	 * Compares one paper to another
+	 * Graphs the number of journal articles the author produced each year
+	 * @param input The author who's data you want to graph
 	 */
-	@Override
-	public int compare(Paper o1, Paper o2) {
-		// TODO Auto-generated method stub
-		return 0;
+	public void graphJournalPerYear(Author input){
+		
+	}
+	
+	/**
+	 * Graphs the number of coauthors for each publication
+	 * @param input The author who's data you want to graph
+	 */
+	public void graphNumberCoauthors(Author input){
+		
 	}
 }
