a9851dd58b191b6d75b16beebebdad5b7753f583
Narkidae <connorbtaylor@gmail.com>
2013-10-09 14:14:10 -0500

    Create PaperCollectionSearchTitleTest.java

---
+++ PaperCollectionSearchTitleTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class PaperCollectionSearchTitleTest {
+
+	@Test
+	public void test() throws IOException {
+		//Make a new collection from file
+		PaperCollection testCollection = new PaperCollection("papers.txt");
+		
+		Paper result = testCollection.searchTitle("Mexual Selection, Resource Distribution, and Population Size in Synthetic Sympatric Speciation");
+		
+		//Test to see if the first paper is the expected paper
+		Assert.assertEquals("Conference Paper // Woehrer, Mark; Hougen, Dean; Schlupp, Ingo // Mexual Selection, Resource Distribution, and Population Size in Synthetic Sympatric Speciation // Subinternational Conference on the Synthesis and Simulation of Living Systems // 135-145 // July 2012 // http://dx.doi.org/10.7551/978-0-262-31050-5-ch020", result.toString());
+	}
+
+}
