baf5976d7f263b17e1634e05b3ef013bae62d9b6
Narkidae <connorbtaylor@gmail.com>
2013-10-09 14:09:47 -0500

    Javadoc

--- Author.java
+++ Author.java
@@ -1,9 +1,9 @@
 
 public class Author {
 
-	/** Last name of the author */
+	/* Last name of the author */
 	private String secondaryName;
-	/** First and middle name(s) of the author */
+	/* First and middle name(s) of the author */
 	private String primaryName;
 	
 	/**
@@ -16,7 +16,7 @@ public class Author {
 	/**
 	 * Preferred constructor, takes the full name of the author in either
 	 * primary, secondary or secondary primary format.
-	 * @param name
+	 * @param name The name of the Author
 	 */
 	public Author(String name)
 	{
