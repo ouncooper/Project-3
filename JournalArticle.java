71699bad50b059264f6c6bcd336a0e73378346b2
Narkidae <connorbtaylor@gmail.com>
2013-10-09 14:11:19 -0500

    Javadoc

--- JournalArticle.java
+++ JournalArticle.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 import java.util.Comparator;
 
-public class JournalArticle extends Paper implements Comparator<Paper>
+public class JournalArticle extends Paper implements Serializable, Comparator<Paper>
 {
 	private static final long serialVersionUID = 3324219143415691342L;
 	
@@ -52,6 +52,9 @@ public class JournalArticle extends Paper implements Comparator<Paper>
 		return getName() + " // " + getAuthors() + " // " + getTitle() + " // " + getSerialTitle() + " // " + getVolumeIssue() + ":" + getPageRange() + " // " + getDate() + " // " + getDOI();
 	}
 
+	/**
+	 * Compares one paper to another
+	 */
 	@Override
 	public int compare(Paper o1, Paper o2) {
 		// TODO Auto-generated method stub
