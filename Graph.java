a2e441536601cdc91175bd168e652847bbb6df02
Narkidae <connorbtaylor@gmail.com>
2013-10-09 14:10:41 -0500

    Javadoc, new methods

--- Graph.java
+++ Graph.java
@@ -7,10 +7,42 @@ public class Graph {
 	public Graph() { }
 	
 	/**
-	 * Draws the histogram.
+	 * Graphs the number of journal articles vs the number of conference papers 
+	 * @param input The author who's data you want to graph
 	 */
-	public void drawGraph() {
+	public void graphType(Author input){
+		
+	}
+	
+	/**
+	 * Graphs the number of publications the author produced each year
+	 * @param input The author who's data you want to graph
+	 */
+	public void graphPublicationsPerYear(Author input){
+		
+	}
+	
+	/**
+	 * Graphs the number of conference papers the author produced each year
+	 * @param input The author who's data you want to graph
+	 */
+	public void graphConferencePerYear(Author input){
 		
 	}
 
+	/**
+	 * Graphs the number of journal articles the author produced each year
+	 * @param input The author who's data you want to graph
+	 */
+	public void graphJournalPerYear(Author input){
+		
+	}
+	
+	/**
+	 * Graphs the number of coauthors for each publication
+	 * @param input The author who's data you want to graph
+	 */
+	public void graphNumberCoauthors(Author input){
+		
+	}
 }
