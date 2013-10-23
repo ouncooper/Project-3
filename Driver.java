e0f5921137a89c393ba6277fb6010bb2d2a6f949
Narkidae <connorbtaylor@gmail.com>
2013-10-23 17:41:27 -0500

    Fixed save option and added help menu

--- Driver.java
+++ Driver.java
@@ -26,18 +26,36 @@ public class Driver
 	 * then creates a PaperCollection that stores the papers read in.
 	 * @param args Command line arguments.
 	 * @throws IOException Error in I/O.
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args) throws IOException, ClassNotFoundException
 	{
 		BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Please input a filepath for the papers file.  Leave blank for default.");
+		System.out.println("Please input a filepath for the papers file.");
 		String input = inputReader.readLine();
 		PaperCollection academicPapers = new PaperCollection(input);
 		
 		do {
 			System.out.println("Please input either a sort criteria or other action command: ");
 			input = inputReader.readLine();
-			if (input.equalsIgnoreCase("BI")) {
+			if (input.equalsIgnoreCase("help")){
+				System.out.println("----- Help Menu -----");
+				System.out.println("------ General ------");
+				System.out.println("E - Exit");
+				System.out.println("-- Sorting Methods --");
+				System.out.println("BI - Sort biblographically");
+				System.out.println("AN - Sort by author name");
+				System.out.println("PT - Sort by paper title");
+				System.out.println("ST - Sort by serial title");
+				System.out.println("CH - Sort chronologically");
+				System.out.println("R - Randomize the papers");
+				System.out.println("------ File IO ------");
+				System.out.println("PS - Print to Screen");
+				System.out.println("PF - Print to File");
+				System.out.println("----- Searching -----");
+				System.out.println("S - Search by title");
+				System.out.println("FA - Search by author\n");
+			} else if (input.equalsIgnoreCase("BI")) {
 				academicPapers.sort("BI");
 			} else if (input.equalsIgnoreCase("AN")) {
 				academicPapers.sort("AN");
@@ -52,10 +70,24 @@ public class Driver
 			} else if (input.equalsIgnoreCase("PS")) {
 				academicPapers.printToScreen();
 			} else if (input.equalsIgnoreCase("PF")) {
-				academicPapers.printTextFile("papersOutput.txt");
+				System.out.println("Please enter the location you would like to save to: ");
+				input = inputReader.readLine();
+				if(input.endsWith(".txt")){
+					System.out.println("Saving as " + input);
+					academicPapers.printTextFile(input);
+				}
+				else if(input.endsWith(".ppr")){
+					System.out.println("Saving as " + input);
+					academicPapers.printBinFile(input);
+				}
+				else{
+					System.out.println("Saving as " + input + ".ppr");
+					academicPapers.printBinFile(input+".ppr");
+				}
+				System.out.println("Saved.");
 			} else if (input.equalsIgnoreCase("S")) {
 				System.out.print("Please input a title to find: ");
-				academicPapers.searchTitle(inputReader.readLine());
+				System.out.println(academicPapers.searchTitle(inputReader.readLine()).toString().replaceAll(" // ", "\n"));
 			} else if (input.equalsIgnoreCase("FA")) {
 				System.out.print("Please input an author to find: ");
 				try {
@@ -63,7 +95,7 @@ public class Driver
 				} catch (ClassNotFoundException e) { }
 			}
 				
-		} while (input != "E");
+		} while (!input.equalsIgnoreCase("E"));
 		
 		System.out.println("Thank you, exiting.");
 	}
