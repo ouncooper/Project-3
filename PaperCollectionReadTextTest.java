c786f08304ba2dbf270ef743e768f032af1aac76
Narkidae <connorbtaylor@gmail.com>
2013-10-23 20:11:14 -0500

    final upload

--- PaperCollectionReadTextTest.java
+++ PaperCollectionReadTextTest.java
@@ -10,16 +10,14 @@ import org.junit.Test;
 public class PaperCollectionReadTextTest {
 
 	@Test
-	public void test() {
+	public void test() throws IOException, ClassNotFoundException {
 		//Filename containing exactly 4 test papers
 		String filepath = "papers.txt";
 
-		PaperCollection testCollection = null;
-		try {
-			testCollection = new PaperCollection(filepath);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		PaperCollection testCollection = new PaperCollection(filepath);
+		
+		//For visual inspection
+		testCollection.printToScreen();
 		
 		//See if there are exactly 4 papers in the collection
 		Assert.assertEquals(testCollection.size(), 4);
